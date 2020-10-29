7b3a39522e13f9543d0f434de62db3df90390ac9
MicImbriani <imbr.mic@gmail.com>
2020-10-29 22:05:27 +0000

    Small changes.

--- U-Net Architecture.py
+++ U-Net Architecture.py
@@ -5,19 +5,22 @@ import torch.nn as nn
 # the section 2 "Network Architecture" of the U-Net paper.
 
 
+
 # The architecture consists of the repeated application of two 3x3 unpadded covolutions.
 # Thus I will abstract and create a function that can be applied to all repetitions.
 # It takes 2 parameters: input channels and output channels. 
 def double_convolution(in_ch, out_ch):
     # It is a normal convolutional network, hence sequential structure.
     convolution = nn.Sequential(
-        nn.Conv2d(in_ch, out_ch, kernel_size=3)
+        nn.Conv2d(in_ch, out_ch, kernel_size=3),
         # inplace=True because there is no need to create and store a copy
-        nn.ReLU(inplace=True)
-        nn.Conv2d(out_ch, out_ch, kernel_size=3)
+        nn.ReLU(inplace=True),
+        nn.Conv2d(out_ch, out_ch, kernel_size=3),
         nn.ReLU(inplace=True)
     )
     return convolution
+
+
 
 class UNet(nn.Module):
     def __init__(self):
@@ -26,12 +29,14 @@ class UNet(nn.Module):
         # Define the MaxPooling 2x2 layer with stride 2.
         self.maxpool =  nn.MaxPool2d(kernel_size=2, stride=2)
         
-        # Define the 5 convolutions. Number of input and output channels are specified on the paper.
+        # Define the 5 downwards convolutions. Number of input and output channels are specified on the paper.
         self.convolution_1 = double_convolution(1, 64)
         self.convolution_2 = double_convolution(64, 128)
         self.convolution_3 = double_convolution(128, 256)
         self.convolution_4 = double_convolution(256, 512)
         self.convolution_5 = double_convolution(512, 1024)
+
+        # Define the 5 upwards convolutions.
 
     # Define a function that will downsample the input image by passing it through all the convolutions and maxpools.
     # Here a "floor" is every 2 conv layers and 1 maxpool layer.
@@ -54,7 +59,7 @@ class UNet(nn.Module):
         
 
 
-        if __name__ == "__main__":
-            image = torch.rand((1,1,572,572))
-            model = UNet()
-            print(model(image))
+if __name__ == "__main__":
+    image = torch.rand((1,1,572,572))
+    model = UNet()
+    print(model(image))
