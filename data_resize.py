fb57c2d0c3a40b343e28e4a129f952745793755c
MicImbriani <imbr.mic@gmail.com>
2021-02-10 12:27:34 +0000

    updated train file, data_resize

--- data_resize.py
+++ data_resize.py
@@ -3,12 +3,13 @@ import glob
 import logging
 
 import pandas as pd
+from sklearn import model_selection
+from torch.utils.data import Dataset
 
 from tqdm import tqdm
 from PIL import Image, ImageFile
 from joblib import Parallel, delayed
-from sklearn import model_selection
-from torch.utils.data import Dataset
+
 from dataset import MyDataset
 
 
@@ -20,7 +21,7 @@ ImageFile.LOAD_TRUCATED_IMAGES = True
 # Configure logging's details
 logging.basicConfig(filename='data_resize.log', level=logging.INFO,
                     format='%(asctime)s:%(levelname)s:%(message)s')
-        
+
 
 
 def resize(input_path, output_dir, size):
@@ -110,27 +111,8 @@ def split_train_validation(input_path, file_name, splits):
     for fold, train_idx, test_idx, in enumerate(kf.split(X=df, y=y)):
         df.loc[:, "k-fold"] = fold
     df.to_csv(os.path.join(input_path, "train_folds.csv"), index=False)
-    # logging.info(f'The dataset train/test split is: ')
-
 
 
 if __name__ == "__main__":
-    train_imgs_path = 'data/original_dataset/Train'
-    train_masks_path = 'data/original_dataset/Train_GT_masks'
-    train_imgs_save_path = 'data/modified_dataset/Train_resized'
-    train_masks_save_path = 'data/modified_dataset/Train_GT_resized'
-
-    test_imgs_path = 'data/original_dataset/Test'
-    test_masks_path = 'data/original_dataset/Test_GT_masks'
-    test_imgs_save_path = 'data/modified_dataset/Test_resized'
-    test_masks_save_path = 'data/modified_dataset/Test_GT_resized'
-
-    size = (572,572)
-    jobs = 10
-
-    resize_train(train_imgs_path, train_imgs_save_path, size, jobs)
-    resize_train(train_masks_path, train_masks_save_path, size, jobs)
-
-    resize_test(test_imgs_path, test_imgs_save_path, size, jobs)
-    resize_test(test_masks_path, test_masks_save_path, size, jobs)
+    
     
--- train.py
+++ train.py
@@ -12,3 +12,29 @@ from tqdm import tqdm
 from eval import eval_net
 from my_U-Net import UNet 
 
+
+
+
+train_imgs_path = 'data/original_dataset/Train'
+train_masks_path = 'data/original_dataset/Train_GT_masks'
+train_imgs_save_path = 'data/modified_dataset/Train_resized'
+train_masks_save_path = 'data/modified_dataset/Train_GT_resized'
+
+test_imgs_path = 'data/original_dataset/Test'
+test_masks_path = 'data/original_dataset/Test_GT_masks'
+test_imgs_save_path = 'data/modified_dataset/Test_resized'
+test_masks_save_path = 'data/modified_dataset/Test_GT_resized'
+
+resize_dimensions = (572,572)
+resize_jobs = 10
+
+
+
+
+
+
+resize_train(train_imgs_path, train_imgs_save_path, size, jobs)
+resize_train(train_masks_path, train_masks_save_path, size, jobs)
+
+resize_test(test_imgs_path, test_imgs_save_path, size, jobs)
+resize_test(test_masks_path, test_masks_save_path, size, jobs)
